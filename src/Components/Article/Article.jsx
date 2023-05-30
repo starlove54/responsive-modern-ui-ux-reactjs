08ecdacac084738b0013c82b8803f4d87009f2f8
Anubhav Negi <anubhavnegi54@gmail.com>
2023-05-31 04:16:20 +0530

    update

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -1,10 +1,17 @@
 import './Article.css';
-
-
-const Article = () => {
+const Article = ({imgUrl,date,text}) => {
   return (
-    <div>
-      
+    <div className='gpt3__blog-container_article'>
+    <div className='gpt3__blog-container_article-image'>
+      <img src={imgUrl} alt="blog image"/>
+    </div>
+    <div className='gpt3__blog-container_article-content'>
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3> 
+      </div>
+      <p>Read Full Article</p>
+    </div>
     </div>
   )
 }
